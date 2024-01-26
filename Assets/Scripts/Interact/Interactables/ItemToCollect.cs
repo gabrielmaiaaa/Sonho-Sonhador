ab2dd978780ebb10b5e4f6ae44e69756990ee06c
MatheusCapucho <matheusfcapucho@gmail.com>
2024-01-26 16:21:40 -0300

    SSD Item & Pedestal

---
+++ Assets/Scripts/Interact/Interactables/ItemToCollect.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemToCollect : MonoBehaviour, IInteractable
+{
+    [SerializeField] private ItemSO itemSO;
+    [SerializeField] private bool _canDelete = false;
+
+    private void Start()
+    {
+        if (itemSO.ItemCollected && _canDelete)
+            Destroy(this.gameObject);
+    }
+    public void Interact()
+    {
+        if (!_canDelete)
+            return;
+
+        itemSO.SetItemCollected(true);
+        SetUI();
+        Destroy(this.gameObject);
+    }
+
+    private void SetUI()
+    {
+        ItensCollected.OnUpdateUI?.Invoke();
+    }
+}
