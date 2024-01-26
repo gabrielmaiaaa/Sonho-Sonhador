b47e4e62324b3d9e0940bc530b72893b6dd615c4
Gabriel Maia <135539097+ConsidereSeGerenciado@users.noreply.github.com>
2024-01-26 14:22:49 -0300

    Maluverse

---
+++ Assets/Scripts/2D 3D/seila.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class seila : MonoBehaviour
+{
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(0f, Camera.main.transform.rotation.eulerAngles.y, 0f);
+    }
+}
