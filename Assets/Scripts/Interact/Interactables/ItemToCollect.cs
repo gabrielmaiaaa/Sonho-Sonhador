9b56508256ab610af96b0684f8770b1b6766df9a
MatheusCapucho <matheusfcapucho@gmail.com>
2024-01-26 16:30:06 -0300

    DOTween & Tutu

--- Assets/Scripts/Interact/Interactables/ItemToCollect.cs
+++ Assets/Scripts/Interact/Interactables/ItemToCollect.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class ItemToCollect : MonoBehaviour, IInteractable
 {
@@ -10,7 +11,13 @@ public class ItemToCollect : MonoBehaviour, IInteractable
     private void Start()
     {
         if (itemSO.ItemCollected && _canDelete)
+        {
+            transform.DOKill();
             Destroy(this.gameObject);
+        }
+
+        transform.DOLocalRotate(new Vector3(0f, 360f, 0f), 2f, RotateMode.FastBeyond360).SetLoops(-1).SetEase(Ease.Linear);
+
     }
     public void Interact()
     {
@@ -19,6 +26,7 @@ public class ItemToCollect : MonoBehaviour, IInteractable
 
         itemSO.SetItemCollected(true);
         SetUI();
+        transform.DOKill();
         Destroy(this.gameObject);
     }
 
